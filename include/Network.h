10f180ffd953623d7f2a5af4a6323820b2404f9c
Tim Sweeney <mainetim@gmail.com>
2022-06-16 10:53:47 -0400

    Add new files.

---
+++ include/Network.h
@@ -0,0 +1,10 @@
+
+// SSID of your WIFI Network
+const char* ssid = "";
+// Password for your WIFI Network
+const char* password =  "";
+ 
+// IP Address of the server keyer.
+const char * host = "";
+
+const unsigned int port = 4120;
